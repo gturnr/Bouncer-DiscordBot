27f64514d656a82a36e5058213fb2fc649c95341
Guy Turner <guyturner797@users.noreply.github.com>
2017-11-25 12:47:23 +0000

    quick update improvements

--- update.py
+++ update.py
@@ -3,28 +3,35 @@ import urllib.request
 
 script_location = 'https://raw.githubusercontent.com/guyturner797/Bouncer-DiscordBot/master/main.py'
 
-
 print('updating BouncerBot...')
 time.sleep(2)
 
+
+print('pulling script')
 try:
-    print('pulling script')
     response = urllib.request.urlopen(script_location)
     data = response.read()
     text = data.decode('utf-8')
     print('script pull completed')
-    os.rename('main.py', 'old.py')
-    updater = open('main.py', 'w')
-    updater.write(text)
-    updater.close()
 
 except:
     print('pull failed. Reverting to current version')
+    log = open('updatelog.txt', 'w')
+    log.write('updated failed')
+    log.close()
+    os.system('python main.py')
+    exit()
 
+if os.path.exists('revisions/old.py') == True:
+    os.remove('revisions/old.py')
 
+os.rename('main.py', 'revisions/old.py')
+updater = open('main.py', 'w')
+updater.write(text)
+updater.close()
 
 log = open('updatelog.txt', 'w')
-
+log.write('updated successfully')
 log.close()
 time.sleep(2)
 
