2473473075c81ff61d7bf0837fecb51fe9adab4f
Guy Turner <guyturner797@users.noreply.github.com>
2017-11-27 10:41:05 +0000

    building improvements for migration to sql

--- dbtools.py
+++ dbtools.py
@@ -18,7 +18,17 @@ def writeServerConfig(server, chat):
 def getServerConfig(server):
     c.execute("SELECT * FROM servers WHERE serverID=?", (server,))
     result = c.fetchall()
-    print(result[0])
+    return(result[0][2])
+
+def backupUser(server, user, nick, roles):
+    c.execute("SELECT * FROM users WHERE serverID=? AND userID=?", (int(server), int(user)))
+    if len(c.fetchall()) == 0:
+        c.execute('INSERT INTO users(userID, serverID, nickname, roles) VALUES(?,?,?,?)', (int(user), int(server), nick, str(roles)))
+    else:
+        c.execute("UPDATE servers SET chatID = ? WHERE serverID = ?", (chat, server))
+    conn.commit()
+
 
 writeServerConfig(10, 5)
+getServerConfig(10)
 conn.close()
