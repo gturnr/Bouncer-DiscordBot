fcb17cfb4460bfbbd9a030955d7af95cc0033440
Guy Turner <guyturner797@users.noreply.github.com>
2017-12-01 18:01:17 +0000

    improved getChannel function

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ client = discord.Client() #creates the Discord client
 global spam_time
 spam_time = 0 #creates a variable, later used to prevent spam and abuse of bot commands
 
+
 def playingStatus(): #function to return the number of servers that the bot is currently running in
     numOfServers = len(client.servers)
     return(str(numOfServers))
+
 
 def getAuth(): #opens a text file and loads the disocrd private key from it
     try:
@@ -17,18 +19,26 @@ def getAuth(): #opens a text file and loads the disocrd private key from it
         key = file.readline()
         file.close()
         return key
-    except: #if the file could not be opened
+    except: #if the file could not be opened, exits to prevent a non-authorised attempt at connecting to discord
         print('Failed to load key file... create a text file called "key.txt" with your auth key')
         exit()
 
+
 def getServerChat(server): #function to recall the preffered server channel from local file storage
     try:
-        channelID = dbtools.getServerConfig(int(server.id))
+        channelID = dbtools.getServerConfig(int(server.id)) #gets the channel ID for a sever
         for pos, channel in enumerate(server.channels): #NEEDS CLEANING UP. gets the correct channel object from the channel id
             if str(channel.id) == str(channelID):
                 return channel
-    except:
-        return
+
+    except IndexError:
+        print('Server not yet configured')
+    except :
+        print('Unknown db error')
+
+    for channel in server.channels: #backup system if channel not yet set, returns first text channel in server (usually the oldest)
+        if str(channel.type) == 'text':
+            return channel
 
 
 async def titleUpdater(): #function to update the 'playing' variable of the bot every X seconds with the correct number of servers
@@ -36,6 +46,7 @@ async def titleUpdater(): #function to update the 'playing' variable of the bot 
         await client.wait_until_ready()
         await client.change_presence(game=discord.Game(name='Keeping ' + playingStatus() + ' server(s) in check'))
         await asyncio.sleep(120) #X value here
+
 
 @client.event
 async def on_ready(): #function to output the client name and id upon successful connection to Discord
@@ -56,39 +67,43 @@ async def on_member_remove(member): #function to run when a member keaves or is 
         if str(role) != '@everyone': #ignores the role @everyone as this does not require reassignment later on
             strRoles.append(str(role.id))
 
-    serverID = str(member.server.id) #gets the server id of the member
+    server_id = str(member.server.id) #gets the server id of the member
 
+    dbtools.backupUser(server_id, member.id, nickname, strRoles)
 
-    if not os.path.exists('servers/' + serverID): #checks if a directory already exists for the server, if not it will create a new one
-        os.makedirs('servers/' + serverID)
+    '''if not os.path.exists('servers/' + server_id): #checks if a directory already exists for the server, if not it will create a new one
+        os.makedirs('servers/' + server_id)
 
-    fileExport = open('servers/' + serverID + '/' + member.id, 'wb') #opens a new file in the sever directory for the member (binary for nicknames)
+    fileExport = open('servers/' + server_id + '/' + member.id, 'wb') #opens a new file in the sever directory for the member (binary for nicknames)
     fileExport.write((nickname + '\n' + str(strRoles)).encode('UTF-8')) #outputs the nickname and list of roles to the file, using UTF-8 encoding for speciaist characters
-    fileExport.close() #closes the file
+    fileExport.close() #closes the file'''
 
     channel = getServerChat(member.server) #calls getServerChat function to return the preferred server channel
-    try: #will attempt to send the following messages (assuming channel does not just return Null)
-        await client.send_message(channel, ('Member ' + member.name + ' has left the server... configuration successfully backed up.')) ##outputs confirmation of backup
-        await client.send_file(channel, 'bye.gif') #uploads a .gif file to the channel
-    except:
-        pass
+    await client.send_message(channel, ('Member ' + member.name + ' has left the server... configuration successfully backed up.')) ##outputs confirmation of backup
+    await client.send_file(channel, 'bye.gif') #uploads a .gif file to the channel
+
 
 @client.event
 async def on_member_join(member): #function run upon a new user joining a server
-    if member == client.user or member.bot == True: #exits the function if the new user is a bot or itself... 
+    if member == client.user == True:
         return
 
     channel = getServerChat(member.server)
     await client.send_message(channel, ('Welcome ' + member.mention + ' to the server!')) #welcome greating to the new member
 
-    try: #will attempt to load member details
+    #try: #will attempt to load member details
+
+    nickname, roles = dbtools.getUser(member.server.id, member.id)
+    print(nickname)
+
+    '''
         details = open('servers/' + member.server.id + '/' + member.id, 'rb') #opens the relevant server member config file
         nickname = details.readline().decode('UTF-8') #loads the nickname (retaining any characters or symbols) 
         roles = ast.literal_eval(details.readline().decode('UTF-8')) #loads the string representation of the roles list and uses ast to transform back into a list type
-        details.close()
+        details.close()'''
 
-    except: #if there were no saved member config then the function exits
-        return
+    #except: #if there were no saved member config then the function exits
+     #   return
 
     await client.change_nickname(member, nickname) #changes the member nickname
     added_roles = [] #list for successful roles
@@ -107,17 +122,13 @@ async def on_member_join(member): #function run upon a new user joining a server
     failed_roles_str = ', '.join(map(str, failed_roles))
     added_roles_str = ', '.join(map(str, added_roles))
 
-    os.remove('servers/' + member.server.id + '/' + member.id) #delete member file
+    #os.remove('servers/' + member.server.id + '/' + member.id) #delete member file
 
-    try: #will attempt to send messages to the set server channel - if not set by the server owner this will fail
-        if len(added_roles) != 0: #if there is at least one entry in the list
-            await client.send_message(channel, ('Member ' + member.name + ' has been reassigned roles: ' + added_roles_str)) #outputs to the chat the roles that have been reassigned
+    if len(added_roles) != 0: #if there is at least one entry in the list
+        await client.send_message(channel, ('Member ' + member.name + ' has been reassigned roles: ' + added_roles_str)) #outputs to the chat the roles that have been reassigned
 
-        if len(failed_roles) != 0 :#if there is at least one entry in the list
-            await client.send_message(channel, ('Please be aware! The following roles could not be reassigned: ' + failed_roles_str)) #outputs to the chat the roles that could not be reassigned (usually permission related issues)
-    except:
-        pass
-
+    if len(failed_roles) != 0 :#if there is at least one entry in the list
+        await client.send_message(channel, ('Please be aware! The following roles could not be reassigned: ' + failed_roles_str)) #outputs to the chat the roles that could not be reassigned (usually permission related issues)
 
 
 @client.event
@@ -182,16 +193,16 @@ async def on_message(message):
         else:
             await client.send_message(message.channel, ("You are not my father..."))
 
-
+    if message.content.startswith('!testchat'):
+        channel = getServerChat(message.server)
+        await client.send_message(channel, ('All good'))
 
 
 @client.event
 async def on_server_join(server): #function to run when the bot joins a new server
-    for channel in server.channels: #loops through every channel in the server
-            if str(channel.type) == 'text': #when the loop finds the first text channel it will output a message and then break from the loop
-                #outputs welcome message and setup details
-                await client.send_message(channel, ('Hi there! My name is kickbot and I manage user roles and nicknames, I can restore these details when someone mysteriously goes missing ;) Here is the deal - to set me up edit my role so it is above the roles you want me to be able to manage! I also need nickname and role permissions! Finally, the server owner needs to type "!setchat" in the text chat you want this bot to use'))
-                break
+    channel = getServerChat(server)
+    await client.send_message(channel, ('Hi there! My name is kickbot and I manage user roles and nicknames, I can restore these details when someone mysteriously goes missing ;) Here is the deal - to set me up edit my role so it is above the roles you want me to be able to manage! I also need nickname and role permissions! Finally, the server owner needs to type "!setchat" in the text chat you want this bot to use'))
+
 
 client.loop.create_task(titleUpdater()) #creates a task to update the 'playing' status
 client.run(getAuth()) #runs the client using the Discord bot token in the file 'key.txt'
